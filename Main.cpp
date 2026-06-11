71f1b2cb0f0dcef2775b5452ac2864d2f8eb6932
Manuela Larissa Stivanin <210542@upf.br>
2026-06-11 18:52:53 -0300

    Fixed some inconsistencies.

--- Main.cpp
+++ Main.cpp
@@ -9,53 +9,57 @@
 
 using namespace std;
 
-void menuPessoa(PessoaFisica& pf, vector<Categoria>& categorias);
-void menuPessoa(PessoaJuridica& pj, vector<Categoria>& categorias);
+void menuPessoaFisica(PessoaFisica& pf, vector<Categoria>& categorias);
+void menuPessoaJuridica(PessoaJuridica& pj, vector<Categoria>& categorias);
 void menuContasPessoa(Pessoa& p, vector<Categoria>& categorias);
 void menuMovimentacoesConta(Conta* conta, vector<Categoria>& categorias);
 
 void menuMovimentacoesConta(Conta* conta, vector<Categoria>& categorias) {
     if (conta == nullptr) {
-        cout << "Conta nao encontrada.\n";
+        cout << "Conta nao encontrada!\n";
         return;
     }
 
     int opcao = 0;
-    while (opcao != 1000) {
-        cout << "\n--- Movimentacoes da Conta " << conta->getNumeroConta() << " ---\n";
-        cout << "1   - Adicionar Movimentacao\n";
-        cout << "2   - Listar Movimentacoes\n";
-        cout << "3   - Remover Movimentacao\n";
-        cout << "1000 - Voltar\n";
-        cout << "Opcao: ";
+    while (opcao != 4) {
+        cout << "\n--- Movimentações da Conta " << conta->getNumeroConta() << " ---\n";
+        cout << "1   - Adicionar Movimentação\n";
+        cout << "2   - Listar Movimentações\n";
+        cout << "3   - Remover Movimentação\n";
+        cout << "4   - Voltar\n";
+        cout << "Opção: ";
         cin >> opcao;
+        cout << "\n";
 
         switch (opcao) {
             case 1: {
+
+                //verifica se existem categorias cadastradas
                 if (categorias.empty()) {
                     cout << "Nenhuma categoria cadastrada.\n";
                     break;
                 }
 
-                int id, categoriaIdx;
+                int id, categoriaId;
                 double valor;
                 string data, tipoMovimentacao, descricao, contraparte;
 
                 cout << "ID: "; cin >> id;
                 cout << "Valor: "; cin >> valor;
                 cout << "Data (DD/MM/YYYY): "; cin >> data;
-                cout << "Tipo (entrada/saida): "; cin >> tipoMovimentacao;
-                cout << "Descricao: "; cin >> descricao;
+                cout << "Tipo (entrada/saída): "; cin >> tipoMovimentacao;
+                cout << "Descrição: "; cin >> descricao;
                 cout << "Contraparte: "; cin >> contraparte;
+                cout << "\n";
 
-                cout << "Categorias disponiveis:\n";
+                cout << "Categorias disponíveis:\n";
                 for (int i = 0; i < (int)categorias.size(); i++) {
                     cout << i + 1 << " - " << categorias[i].getNome() << "\n";
                 }
-                cout << "Categoria (numero): "; cin >> categoriaIdx;
+                cout << "Categoria: "; cin >> categoriaId;
 
-                if (categoriaIdx < 1 || categoriaIdx > (int)categorias.size()) {
-                    cout << "Categoria invalida.\n";
+                if (categoriaId < 1 || categoriaId > (int)categorias.size()) {
+                    cout << "Categoria inválida!\n";
                     break;
                 }
 
@@ -66,9 +70,9 @@ void menuMovimentacoesConta(Conta* conta, vector<Categoria>& categorias) {
                     tipoMovimentacao,
                     descricao,
                     contraparte,
-                    categorias[categoriaIdx - 1]
+                    categorias[categoriaId - 1]
                 ));
-                cout << "Movimentacao adicionada!\n";
+                cout << "Movimentação adicionada!\n";
                 break;
             }
             case 2:
@@ -76,14 +80,14 @@ void menuMovimentacoesConta(Conta* conta, vector<Categoria>& categorias) {
                 break;
             case 3: {
                 int id;
-                cout << "ID da movimentacao a remover: "; cin >> id;
+                cout << "ID da movimentação a remover: "; cin >> id;
                 conta->removerMovimentacao(id);
                 break;
             }
-            case 1000:
+            case 4:
                 break;
             default:
-                cout << "Opcao invalida.\n";
+                cout << "Opção inválida.\n";
         }
     }
 }
@@ -98,9 +102,10 @@ void menuContasPessoa(Pessoa& p, vector<Categoria>& categorias) {
         cout << "3   - Selecionar Conta\n";
         cout << "4   - Ver Saldo Total\n";
         cout << "5   - Ver Saldo por Categoria\n";
-        cout << "6 - Voltar\n";
-        cout << "Opcao: ";
+        cout << "6   - Voltar\n";
+        cout << "Opção: ";
         cin >> opcao;
+        cout << "\n";
 
         switch (opcao) {
             case 1: {
@@ -108,11 +113,12 @@ void menuContasPessoa(Pessoa& p, vector<Categoria>& categorias) {
                 string instituicao, numeroConta, chavePix, tipoConta;
                 double saldo;
                 cout << "ID: "; cin >> id;
-                cout << "Instituicao: "; cin >> instituicao;
-                cout << "Numero da Conta: "; cin >> numeroConta;
+                cout << "Instituição: "; cin >> instituicao;
+                cout << "Número da Conta: "; cin >> numeroConta;
                 cout << "Chave Pix: "; cin >> chavePix;
                 cout << "Saldo inicial: "; cin >> saldo;
                 cout << "Tipo da Conta: "; cin >> tipoConta;
+                cout << "\n";
                 p.adicionarConta(Conta(id, instituicao, numeroConta, chavePix, saldo, tipoConta));
                 cout << "Conta adicionada!\n";
                 break;
@@ -123,12 +129,15 @@ void menuContasPessoa(Pessoa& p, vector<Categoria>& categorias) {
             }
             case 3: {
                 int idConta;
+
+                cout << "Contas disponíveis:\n";
+                p.listarContas();
                 cout << "ID da conta: ";
                 cin >> idConta;
 
                 Conta* conta = p.encontrarContaPorId(idConta);
                 if (conta == nullptr) {
-                    cout << "Conta nao encontrada.\n";
+                    cout << "Conta nao encontrada!\n";
                     break;
                 }
 
@@ -141,33 +150,33 @@ void menuContasPessoa(Pessoa& p, vector<Categoria>& categorias) {
             }
             case 5: {
                 if (categorias.empty()) {
-                    cout << "Nenhuma categoria cadastrada.\n";
+                    cout << "Nenhuma categoria cadastrada!\n";
                     break;
                 }
-                cout << "Categorias disponiveis:\n";
+                cout << "Categorias disponíveis:\n";
                 for (int i = 0; i < (int)categorias.size(); i++) {
                     cout << i + 1 << " - " << categorias[i].getNome() << "\n";
                 }
                 int idx;
                 string tipo;
-                cout << "Selecione a categoria (numero): "; cin >> idx;
-                cout << "Tipo (entrada/saida): "; cin >> tipo;
+                cout << "Selecione a categoria: "; cin >> idx;
+                cout << "Tipo (entrada/saída): "; cin >> tipo;
                 if (idx >= 1 && idx <= (int)categorias.size()) {
                     p.exibirSaldoCategoria(categorias[idx - 1], tipo);
                 } else {
-                    cout << "Categoria invalida.\n";
+                    cout << "Categoria inválida!\n";
                 }
                 break;
             }
-            case 1000:
+            case 6:
                 break;
             default:
-                cout << "Opcao invalida.\n";
+                cout << "Opcao inválida.\n";
         }
     }
 }
 
-void menuPessoa(PessoaFisica& pf, vector<Categoria>& categorias) {
+void menuPessoaFisica(PessoaFisica& pf, vector<Categoria>& categorias) {
     int opcao = 0;
     while (opcao != 1000) {
         cout << "\n--- " << pf.getNome() << " ---\n";
@@ -176,9 +185,10 @@ void menuPessoa(PessoaFisica& pf, vector<Categoria>& categorias) {
         cout << "3   - Adicionar Meta\n";
         cout << "4   - Listar Metas\n";
         cout << "5   - Remover Meta\n";
-        cout << "1000 - Voltar\n";
+        cout << "6   - Voltar\n";
         cout << "Opcao: ";
         cin >> opcao;
+        cout << "\n";
 
         switch (opcao) {
             case 1:
@@ -192,10 +202,11 @@ void menuPessoa(PessoaFisica& pf, vector<Categoria>& categorias) {
                 double orcamento, valorAtual;
                 string prazo, descricao;
                 cout << "ID: "; cin >> id;
-                cout << "Descricao: "; cin >> descricao;
-                cout << "Orcamento: "; cin >> orcamento;
+                cout << "Descrição: "; cin >> descricao;
+                cout << "Orçamento: "; cin >> orcamento;
                 cout << "Valor Atual: "; cin >> valorAtual;
                 cout << "Prazo: "; cin >> prazo;
+                cout << "\n";
                 pf.adicionarMeta(Meta(id, orcamento, prazo, descricao, valorAtual));
                 cout << "Meta adicionada!\n";
                 break;
@@ -210,26 +221,27 @@ void menuPessoa(PessoaFisica& pf, vector<Categoria>& categorias) {
                 cout << "Meta removida (se existia).\n";
                 break;
             }
-            case 1000:
+            case 6:
                 break;
             default:
-                cout << "Opcao invalida.\n";
+                cout << "Opcao inválida.\n";
         }
     }
 }
 
-void menuPessoa(PessoaJuridica& pj, vector<Categoria>& categorias) {
+void menuPessoaJuridica(PessoaJuridica& pj, vector<Categoria>& categorias) {
     int opcao = 0;
-    while (opcao != 1000) {
+    while (opcao != 6) {
         cout << "\n--- " << pj.getNome() << " ---\n";
         cout << "1   - Exibir Dados\n";
         cout << "2   - Gerenciar Contas\n";
         cout << "3   - Adicionar Meta\n";
         cout << "4   - Listar Metas\n";
         cout << "5   - Remover Meta\n";
-        cout << "1000 - Voltar\n";
+        cout << "6   - Voltar\n";
         cout << "Opcao: ";
         cin >> opcao;
+        cout << "\n";
 
         switch (opcao) {
             case 1:
@@ -243,10 +255,11 @@ void menuPessoa(PessoaJuridica& pj, vector<Categoria>& categorias) {
                 double orcamento, valorAtual;
                 string prazo, descricao;
                 cout << "ID: "; cin >> id;
-                cout << "Descricao: "; cin >> descricao;
-                cout << "Orcamento: "; cin >> orcamento;
+                cout << "Descrição: "; cin >> descricao;
+                cout << "Orçamento: "; cin >> orcamento;
                 cout << "Valor Atual: "; cin >> valorAtual;
                 cout << "Prazo: "; cin >> prazo;
+                cout << "\n";
                 pj.adicionarMeta(Meta(id, orcamento, prazo, descricao, valorAtual));
                 cout << "Meta adicionada!\n";
                 break;
@@ -261,10 +274,10 @@ void menuPessoa(PessoaJuridica& pj, vector<Categoria>& categorias) {
                 cout << "Meta removida (se existia).\n";
                 break;
             }
-            case 1000:
+            case 6:
                 break;
             default:
-                cout << "Opcao invalida.\n";
+                cout << "Opção inválida.\n";
         }
     }
 }
@@ -275,25 +288,26 @@ int main() {
     vector<Categoria> categorias;
 
     //categorias padrão
-    categorias.push_back(Categoria(1, "Alimentacao", true));
+    categorias.push_back(Categoria(1, "Alimentação", true));
     categorias.push_back(Categoria(2, "Transporte", true));
     categorias.push_back(Categoria(3, "Moradia", true));
     categorias.push_back(Categoria(4, "Lazer", true));
-    categorias.push_back(Categoria(5, "Saude", true));
+    categorias.push_back(Categoria(5, "Saúde", true));
 
     int n = 0;
     while (n != 6) {
         cout << "\n-------------------------------\n";
         cout << "      Controle Financeiro\n";
         cout << "-------------------------------\n";
-        cout << "1   - Criar Pessoa Fisica\n";
-        cout << "2   - Criar Pessoa Juridica\n";
+        cout << "1   - Criar Pessoa Física\n";
+        cout << "2   - Criar Pessoa Jurídica\n";
         cout << "3   - Listar Pessoas\n";
         cout << "4   - Selecionar Pessoa\n";
         cout << "5   - Gerenciar Categorias\n";
-        cout << "6 - Sair\n";
-        cout << "Digite sua opcao: ";
+        cout << "6   - Sair\n";
+        cout << "Digite sua opção: ";
         cin >> n;
+        cout << "\n";
 
         switch (n) {
             case 1: {
@@ -305,6 +319,7 @@ int main() {
                 cout << "Telefone: "; cin >> telefone;
                 cout << "CPF: "; cin >> cpf;
                 cout << "Data de Nascimento (DD/MM/YYYY): "; cin >> dataNascimento;
+                cout << "\n";
                 pessoasFisicas.push_back(PessoaFisica(id, nome, email, telefone, cpf, dataNascimento));
                 cout << "Pessoa " << pessoasFisicas.back().getNome() << " criada!\n";
                 break;
@@ -318,17 +333,18 @@ int main() {
                 cout << "Telefone: "; cin >> telefone;
                 cout << "CNPJ: "; cin >> cnpj;
                 cout << "Razao Social: "; cin >> razaoSocial;
+                cout << "\n";
                 pessoasJuridicas.push_back(PessoaJuridica(id, nome, email, telefone, cnpj, razaoSocial));
                 cout << "Pessoa " << pessoasJuridicas.back().getNome() << " criada!\n";
                 break;
             }
             case 3: {
-                cout << "\n--- Pessoas Fisicas ---\n";
+                cout << "\n--- Pessoas Físicas ---\n";
                 for (auto& pf : pessoasFisicas) {
                     pf.exibirDados();
                     cout << "---\n";
                 }
-                cout << "\n--- Pessoas Juridicas ---\n";
+                cout << "\n--- Pessoas Jurídicas ---\n";
                 for (auto& pj : pessoasJuridicas) {
                     pj.exibirDados();
                     cout << "---\n";
@@ -336,24 +352,24 @@ int main() {
                 break;
             }
             case 4: {
-                cout << "\n--- Pessoas Fisicas ---\n";
+                cout << "\n--- Pessoas Físicas ---\n";
                 for (auto& pf : pessoasFisicas) {
                     cout << "ID: " << pf.getId() << " | Nome: " << pf.getNome() << "\n";
                 }
-                cout << "\n--- Pessoas Juridicas ---\n";
+                cout << "\n--- Pessoas Jurídicas ---\n";
                 for (auto& pj : pessoasJuridicas) {
                     cout << "ID: " << pj.getId() << " | Nome: " << pj.getNome() << "\n";
                 }
 
                 int tipo, idPessoa;
-                cout << "\n1 - Pessoa Fisica | 2 - Pessoa Juridica\nTipo: "; cin >> tipo;
+                cout << "\n1 - Pessoa Física | 2 - Pessoa Jurídica\nTipo: "; cin >> tipo;
                 cout << "ID da pessoa: "; cin >> idPessoa;
 
                 if (tipo == 1) {
                     bool encontrou = false;
                     for (auto& pf : pessoasFisicas) {
                         if (pf.getId() == idPessoa) {
-                            menuPessoa(pf, categorias);
+                            menuPessoaFisica(pf, categorias);
                             encontrou = true;
                             break;
                         }
@@ -363,12 +379,12 @@ int main() {
                     bool encontrou = false;
                     for (auto& pj : pessoasJuridicas) {
                         if (pj.getId() == idPessoa) {
-                            menuPessoa(pj, categorias);
+                            menuPessoaJuridica(pj, categorias);
                             encontrou = true;
                             break;
                         }
                     }
-                    if (!encontrou) cout << "Pessoa nao encontrada.\n";
+                    if (!encontrou) cout << "Pessoa não encontrada.\n";
                 }
                 break;
             }
@@ -378,8 +394,9 @@ int main() {
                     cout << "\n--- Categorias ---\n";
                     cout << "1   - Adicionar Categoria\n";
                     cout << "2   - Listar Categorias\n";
-                    cout << "3 - Voltar\n";
-                    cout << "Opcao: "; cin >> opcaoCat;
+                    cout << "3   - Voltar\n";
+                    cout << "Opção: "; cin >> opcaoCat;
+                    cout << "\n";
 
                     if (opcaoCat == 1) {
                         int id;
@@ -401,7 +418,7 @@ int main() {
                 cout << "Saindo...\n";
                 break;
             default:
-                cout << "Opcao invalida.\n";
+                cout << "Opcao inválida.\n";
         }
     }
 
